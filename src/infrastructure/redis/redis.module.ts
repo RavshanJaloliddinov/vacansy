440b8efd31c0be3c67533f6bdb35e744b1d3c58c
Ravshan <jaloliddinov008@gmail.com>
2025-03-25 11:27:37 +0500

    1

--- src/infrastructure/redis/redis.module.ts
+++ src/infrastructure/redis/redis.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
-import { config } from 'src/config';
 import { RedisCacheService } from './redis.service';
+import { config } from 'src/config';
 
 @Module({
     imports: [
         ConfigModule,
         NestRedisModule.forRoot({
             type: 'single',
-            url: config.REDIS_URL
+            url: config.REDIS_URL,
+            ...(config.REDIS_URL.startsWith('rediss://') && { tls: {} }), // TLS faqat `rediss://` bo‘lsa qo‘shiladi
         }),
     ],
     providers: [RedisCacheService],
